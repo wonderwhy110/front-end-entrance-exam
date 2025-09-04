a951e8dc9aefc491268051e121c3b3450137d53e
Daria <kurletova@gmail.com>
2025-09-04 17:15:01 +0300

    final zoom fix

--- js/resume-to-pdf.js
+++ js/resume-to-pdf.js
@@ -2,17 +2,21 @@ document.addEventListener("DOMContentLoaded", function () {
   const downloadBtn = document.querySelector(".btn-download");
 
   downloadBtn.addEventListener("click", async function () {
+    const isDesktop = window.innerWidth > 1024;
     try {
       downloadBtn.disabled = true;
       const element = document.getElementById("app");
       const originalPadding = element.style.paddingBottom;
       element.style.paddingBottom = "20px";
       const canvas = await html2canvas(element, {
-        scale: 2,
+        scale: isDesktop ? 4 : 2,
         useCORS: true,
         allowTaint: true,
         logging: true,
-        windowHeight: element.scrollHeight + 100, 
+        width: element.scrollWidth * (isDesktop ? 2 : 1),
+        height: element.scrollHeight * (isDesktop ? 2 : 1),
+        windowWidth: element.scrollWidth * (isDesktop ? 2 : 1),
+        windowHeight: element.scrollHeight * (isDesktop ? 2 : 1) + 200,
         onclone: function (clonedDoc) {
           clonedDoc.getElementById("app").style.paddingBottom = "20px";
         },
@@ -29,14 +33,24 @@ document.addEventListener("DOMContentLoaded", function () {
       const pageWidth = pdf.internal.pageSize.getWidth();
       const pageHeight = pdf.internal.pageSize.getHeight();
       const imgProps = pdf.getImageProperties(imgData);
-      const imgWidth = pageWidth - 10; 
+
+      const imgWidth = isDesktop ? pageWidth * 1 : pageWidth - 10;
+      const xPosition = isDesktop ? 0 : 5;
       const imgHeight = (imgProps.height * imgWidth) / imgProps.width;
 
       let heightLeft = imgHeight;
-      let position = 5; 
-      let pageNumber = 1;
-      pdf.addImage(imgData, "PNG", 5, position, imgWidth, imgHeight);
+      let position = 5;
+
+      // Первая страница
+      pdf.addImage(imgData, "PNG", xPosition, position, imgWidth, imgHeight);
       heightLeft -= pageHeight;
+
+      while (heightLeft > 0) {
+        pdf.addPage();
+        position = position - pageHeight; // Правильное смещение
+        pdf.addImage(imgData, "PNG", xPosition, position, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+      }
 
       pdf.save("моё_резюме.pdf");
     } catch (error) {
